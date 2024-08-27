2d8076cc7e02c4b484189855541e664e643b61ee
EngineersWorld457 <sanjaygnair777@gmail.com>
2024-08-27 07:40:17 -0700

    forgot password nodemailer integration

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -78,7 +78,7 @@ userSchema.methods.createPasswordResetToken = async function () {
     .createHash('sha256')
     .update(resetToken)
     .digest('hex');
-  this.passwordResetExpires = Date.now() + 30 * 60 * 1000; // 10 minutes
+  this.passwordResetExpires = Date.now() + 30 * 60 * 1000; // 30 minutes
   return resetToken;
 };
 //Export the model
